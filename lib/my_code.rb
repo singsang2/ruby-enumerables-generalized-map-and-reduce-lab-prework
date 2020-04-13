2aaab9f2c8b0a797df2cd327a1aea0bec2f1edb3
Sung Bae <singsang2@Sungs-iMac.local>
2020-04-13 13:16:34 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+require "pry"
+def map(array)
+  i = 0
+  new_array = Array.new
+  while i<array.length
+    new_array.push(yield array[i])
+    i+=1
+  end
+  new_array
+end
+#binding.pry
+
+def reduce(array, starting_point = 0)
+  i = 1
+  starting_point == 0 ? a = array[0] : a = array[0] + starting_point
+  while i < array.length
+    a = yield [a, array[i]]
+    i += 1
+  end
+  a
+end
+#binding.pry
